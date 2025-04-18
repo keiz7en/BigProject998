8486f553ffc6ced1803be3c4807e0e3b96a0871c
keiz7en <keizenfaysal@gmail.com>
2025-04-19 02:03:59 +0600

    FirstCommit

--- src/main/java/com/example/bigproject998/MainRunner.java
+++ src/main/java/com/example/bigproject998/MainRunner.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainRunner extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainRunner.class.getResource(""));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainRunner.class.getResource("Starter.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
